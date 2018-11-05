97366266c49f510ab0e6f7be2ec3940a5a024e58
ilyashusain <44235768+ilyashusain@users.noreply.github.com>
2018-11-05 17:07:10 +0000

    Update app.go

--- app.go
+++ app.go
@@ -1,14 +1,14 @@
 package main
 
-import (
+import (                                   #importing libraries
   "log"
   "net/http"
 )
 
-func main() {
-  fs := http.FileServer(http.Dir("./static"))
-  http.Handle("/", fs)
+func main() {                                      #FileServer function: responds to all http requests
+  fs := http.FileServer(http.Dir("./static"))       #handle function: registers FileServer as handler of all requests
+  http.Handle("/", fs)                              #"/" matches all request paths
 
   log.Println("Listening...")
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":8080", nil)               #Listen on a port (in this case port 8080) and thus accept connections to internet
 }
